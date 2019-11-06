a0d88097b94ce18c2cec9b2e6ba58815a35eee1d
Roberto A. Serrano Rojo <raulclljs70@gmail.com>
2019-11-06 10:03:34 -0600

    Agregar archivos de proyecto.

---
+++ veiculoInterfaz/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace veiculoInterfaz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCarro = new System.Windows.Forms.Button();
+            this.btnBrazo = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btnCarro
+            // 
+            this.btnCarro.Location = new System.Drawing.Point(12, 196);
+            this.btnCarro.Name = "btnCarro";
+            this.btnCarro.Size = new System.Drawing.Size(110, 54);
+            this.btnCarro.TabIndex = 1;
+            this.btnCarro.Text = "Carro";
+            this.btnCarro.UseVisualStyleBackColor = true;
+            // 
+            // btnBrazo
+            // 
+            this.btnBrazo.Location = new System.Drawing.Point(12, 100);
+            this.btnBrazo.Name = "btnBrazo";
+            this.btnBrazo.Size = new System.Drawing.Size(110, 54);
+            this.btnBrazo.TabIndex = 2;
+            this.btnBrazo.Text = "Brazo";
+            this.btnBrazo.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(257, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(257, 196);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(63, 50);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(326, 149);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(63, 50);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(188, 149);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(63, 50);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(326, 93);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(63, 50);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(188, 93);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(63, 50);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 475);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnBrazo);
+            this.Controls.Add(this.btnCarro);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCarro;
+        private System.Windows.Forms.Button btnBrazo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
